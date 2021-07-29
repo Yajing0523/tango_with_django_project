5151ad175cdb8db2b2f721ebb18a39d06a4c9e8f
thisisyj-s📻 <2533680S@student.gla.ac.uk>
2021-07-29 14:59:13 +0100

    chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
 def index(request):
- return HttpResponse("Rango says here is the about page")
+    # Refer to the TwD book for more information on how this updated view works.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    # Spoiler: you don't need to pass a context dictionary here.
+    return render(request, 'rango/about.html')
